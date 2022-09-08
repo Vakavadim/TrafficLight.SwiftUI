07072c1c56a84355f497aa76146ee8a4bc3a5648
Vadim Gamzaev <Vadosbox@gmail.com>
2022-09-08 15:38:47 +0300

    Initial Commit

---
+++ TrafficLight.SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight.SwiftUI
+//
+//  Created by Вадим Гамзаев on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
